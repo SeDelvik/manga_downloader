86c6e95d4bdf2dc2d29298b1bba1a6769a0a7899
Nastya <klybnika4749@gmail.com>
2024-06-28 22:13:41 +0300

    Настройка JS и CSS  -

--- static/main.js
+++ static/main.js
@@ -3,8 +3,21 @@ function hi(text) {
 };
 
 function select_all() {
-  let inputs = document.getElementsByTagName('input');
-  for (let i = 0;i<inputs.length;i++){
-    inputs[i].checked = true;
-  }
+    let funky_button = document.getElementById('funky_button');
+    let inputs = document.getElementsByTagName('input');
+    for (let i = 0;i<inputs.length;i++){
+        inputs[i].checked = true;
+    }
+    funky_button.setAttribute("onClick","deselect_all()");
+    funky_button.innerHTML = "Отменить";
 };
+
+function deselect_all(){
+    let funky_button = document.getElementById('funky_button');
+    let inputs = document.getElementsByTagName('input');
+    for (let i = 0;i<inputs.length;i++){
+        inputs[i].checked = false;
+    }
+    funky_button.setAttribute("onClick","select_all()");
+    funky_button.innerHTML = "Выделить всё";
+}
